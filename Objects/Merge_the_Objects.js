091fd9b7726e9efafbb92e1d109b3b6877dee4b6
Neelesh Ranjan <neeleshranjan95@gmail.com>
2022-07-14 00:10:07 +0530

    JS Assignment

---
+++ Objects/Merge_the_Objects.js
@@ -0,0 +1,8 @@
+
+function Check(obj1,obj2) {
+  
+    let merge = Object.assign(obj1,obj2) ;
+    return merge;
+    
+}
+
---
+++ Objects/Sum_of_Object_Members.js
@@ -0,0 +1,6 @@
+
+function Check(obj1) {
+    return(obj1.p1 + obj1.p2 + obj1.p3);
+}
+
+
